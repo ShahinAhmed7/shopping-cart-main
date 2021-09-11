ab41f36c65f67a16c4ab4a48a25528b01f09c059
ShahinAhmed7 <meshahin007@gmail.com>
2021-09-12 00:35:04 +0600

    Update js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 
+
+// Increas decrease in Function
+function updateCaseNumber(isIncreasing){
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber) + 1;
+    } 
+    
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // Update case Total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+
+};
+
+
+
+// Increase case value
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+});
+
+
+
+//decrease case value
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+    //caseInput.value = parseInt(caseNumber) - 1;
+});
